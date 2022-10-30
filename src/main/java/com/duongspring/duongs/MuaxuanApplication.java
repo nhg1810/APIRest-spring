21f593865e4d58d1a4a36cdad1761883957e7169
= <=>
2022-10-30 19:54:41 +0700

    add lan 2

---
+++ src/main/java/com/duongspring/duongs/MuaxuanApplication.java
@@ -0,0 +1,13 @@
+package com.duongspring.duongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuaxuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuaxuanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duongspring/duongs/MuaxuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duongspring.duongs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuaxuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
